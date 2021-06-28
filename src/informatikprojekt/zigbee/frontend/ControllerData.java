ecee551fb9187769f5cea6172b190d7c8912498b
CreeperShift <henkes@stud.fra-uas.de>
2021-06-28 13:45:00 +0200

    Revert "Revert "Implemented Data Tabview""
    This reverts commit 9e2fa4ce

--- src/informatikprojekt/zigbee/frontend/ControllerData.java
+++ src/informatikprojekt/zigbee/frontend/ControllerData.java
@@ -1,13 +1,39 @@
 package informatikprojekt.zigbee.frontend;
 
+import informatikprojekt.zigbee.backend.SensorData;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import jfxtras.styles.jmetro.JMetroStyleClass;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerData implements Initializable {
+
+    public TableView table;
+    public static ControllerData INSTANCE = null;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+        table.getStyleClass().add(JMetroStyleClass.BACKGROUND);
+        TableColumn<SensorData, String> columnTime = new TableColumn<>("Time");
+        columnTime.setCellValueFactory(new PropertyValueFactory<>("formattedTime"));
+        table.getColumns().add(columnTime);
+        INSTANCE = this;
+    }
+
+    public void setupData() {
+
+        if (ControllerBase.INSTANCE.isConnected()) {
+            table.getItems().add(new SensorData("test", 0, 0, 0, 0));
+            table.getItems().add(new SensorData("test2", 0, 0, 0, 0));
+            table.getItems().add(new SensorData("test3", 0, 0, 0, 0));
+            System.out.println("test");
+
+        }
+
+
     }
 }
--- src/informatikprojekt/zigbee/frontend/ControllerGraph.java
+++ src/informatikprojekt/zigbee/frontend/ControllerGraph.java
@@ -1,13 +1,17 @@
 package informatikprojekt.zigbee.frontend;
 
 import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+import jfxtras.styles.jmetro.JMetroStyleClass;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerGraph implements Initializable {
+    public VBox box;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        box.getStyleClass().add(JMetroStyleClass.BACKGROUND);
     }
 }
