85c787e34c98bda275a8f93eef2e32a41d09a9da
hakan <Hakan-m@outlook.de>
2021-07-02 23:52:25 +0200

    Database Connection

---
+++ src/informatikprojekt/zigbee/backend/DataBaseManager.java
@@ -0,0 +1,27 @@
+package informatikprojekt.zigbee.backend;
+
+import java.sql.*;
+
+
+public class DataBaseManager {
+
+
+    public static void main(String args[]) {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sensordaten","root","123");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from test");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getInt(3));
+            }
+            conn.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
