0836c78eb8a8a8bd50cf7d846cd4473201ec0e82
CreeperShift <henkes@stud.fra-uas.de>
2021-07-07 21:21:36 +0200

    Added device tooltip

--- src/informatikprojekt/zigbee/frontend/Device.java
+++ src/informatikprojekt/zigbee/frontend/Device.java
@@ -6,12 +6,16 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
+import org.controlsfx.control.PopOver;
 
 public class Device {
     private static int deviceIDs = 1;
     private final int id;
     private final Text text;
     private final Circle circle;
+    private PopOver popOver;
+
 
     public Device(double x, double y) {
         this(deviceIDs, x, y);
@@ -20,6 +24,11 @@ public class Device {
 
     public Device(int id, double x, double y) {
         this.id = id;
+        popOver = new PopOver();
+        popOver.setDetachable(false);
+        popOver.setAnimated(true);
+        popOver.setX(x);
+        popOver.setY(y);
         circle = new Circle();
         circle.setCenterX(x);
         circle.setCenterY(y);
@@ -32,6 +41,26 @@ public class Device {
         text.setTextAlignment(TextAlignment.CENTER);
         text.setX(x - 5);
         text.setY(y + 6);
+        text.setMouseTransparent(true);
+
+        circle.setOnMouseClicked(event -> {
+            if (popOver != null && !popOver.isDetached()) {
+                popOver.hide();
+            }
+
+            if (event.getClickCount() == 2) {
+                if (popOver != null && popOver.isShowing()) {
+                    popOver.hide(Duration.ZERO);
+                }
+            }
+            if (popOver != null) {
+                popOver.setArrowLocation(PopOver.ArrowLocation.RIGHT_CENTER);
+                popOver.show(ControllerRoomView.INSTANCE.drawingArea, event.getX() + 440, event.getY() + 88);
+            }
+
+
+        });
+
     }
 
     public void addTo(AnchorPane anchorPane) {
