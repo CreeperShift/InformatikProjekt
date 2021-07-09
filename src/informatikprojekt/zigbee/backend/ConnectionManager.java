322553904aab98349cfc51901ec60a45e957941c
CreeperShift <henkes@stud.fra-uas.de>
2021-07-09 15:54:40 +0200

    Updated sqlite create queries

--- src/informatikprojekt/zigbee/backend/ConnectionManager.java
+++ src/informatikprojekt/zigbee/backend/ConnectionManager.java
@@ -44,76 +44,92 @@ public class ConnectionManager {
 
     public static void firstRun() {
         try {
-            String createQuery = """
-                    create table if not exists dataset
-                    (
-                    	id integer
-                    		primary key,
-                    	dateRecorded date,
-                    	timeRecorded time
-                    );
-
-                    create table if not exists room
-                    (
-                    \tid integer
-                    \t\tconstraint room_pk
-                    \t\t\tprimary key,
-                    \troomName varchar(50) not null,
-                    \tcreated datetime
-                    );
-
-                    create table if not exists device
-                    (
-                    \tid integer
-                    \t\tconstraint device_pk
-                    \t\t\tprimary key,
-                    \tnetworkID int,
-                    \tx double(30),
-                    \ty double(30),
-                    \troom_FK int
-                    \t\treferences room
-                    \t\t\ton update cascade on delete cascade
-                    );
-
-                    create table if not exists data
-                    (
-                    \tdataID integer
-                    \t\tprimary key,
-                    \tdataSetID integer
-                    \t\treferences dataset,
-                    \tsensor varchar(50) default 'Unknown',
-                    \tdataType varchar(50) default 'Unknown',
-                    \tdataValue float,
-                    \tdevice_FK int
-                    \t\treferences device
-                    \t\t\ton update cascade on delete cascade
-                    );
-
-                    create table if not exists roomPoints
-                    (
-                    \tid integer
-                    \t\tconstraint roomPoints_pk
-                    \t\t\tprimary key,
-                    \tx double(30),
-                    \ty double(30),
-                    \troomID_FK int
-                    \t\treferences room
-                    \t\t\ton update cascade on delete cascade
-                    );
-
-                    create table if not exists graph
-                    (
-                    \troomPoint_FK int
-                    \t\treferences roomPoints
-                    \t\t\ton update cascade on delete cascade,
-                    \tconnected_FK int
-                    \t\treferences roomPoints
-                    \t\t\ton update cascade on delete cascade,
-                    \tconstraint graph_pk
-                    \t\tprimary key (roomPoint_FK, connected_FK)
-                    );
-
-                    """;
+            String createQuery = "create table if not exists room\n" +
+                    "(\n" +
+                    "\tid integer\n" +
+                    "\t\tconstraint room_pk\n" +
+                    "\t\t\tprimary key,\n" +
+                    "\troomName varchar(50) not null,\n" +
+                    "\tcreated datetime\n" +
+                    ");\n" +
+                    "\n" +
+                    "create table if not exists device\n" +
+                    "(\n" +
+                    "\tid integer\n" +
+                    "\t\tconstraint device_pk\n" +
+                    "\t\t\tprimary key,\n" +
+                    "\tnetworkID int,\n" +
+                    "\tx double(30),\n" +
+                    "\ty double(30),\n" +
+                    "\troom_FK int\n" +
+                    "\t\treferences room\n" +
+                    "\t\t\ton update cascade on delete cascade\n" +
+                    ");\n" +
+                    "\n" +
+                    "create table if not exists recording\n" +
+                    "(\n" +
+                    "\tid integer\n" +
+                    "\t\tconstraint recording_pk\n" +
+                    "\t\t\tprimary key,\n" +
+                    "\troom_FK int\n" +
+                    "\t\treferences room\n" +
+                    "\t\t\ton update cascade on delete cascade,\n" +
+                    "\tdateStarted date,\n" +
+                    "\ttimeStarted time\n" +
+                    ");\n" +
+                    "\n" +
+                    "create table if not exists dataset\n" +
+                    "(\n" +
+                    "\tid integer\n" +
+                    "\t\tprimary key,\n" +
+                    "\tdateRecorded date,\n" +
+                    "\ttimeRecorded time,\n" +
+                    "\trecording_FK int\n" +
+                    "\t\treferences recording\n" +
+                    "\t\t\ton update cascade on delete cascade\n" +
+                    ");\n" +
+                    "\n" +
+                    "create table if not exists data\n" +
+                    "(\n" +
+                    "\tdataID integer\n" +
+                    "\t\tprimary key,\n" +
+                    "\tdataSetID integer\n" +
+                    "\t\treferences dataset,\n" +
+                    "\tsensor varchar(50) default 'Unknown',\n" +
+                    "\tdataType varchar(50) default 'Unknown',\n" +
+                    "\tdataValue float,\n" +
+                    "\tdevice_FK int\n" +
+                    "\t\treferences device\n" +
+                    "\t\t\ton update cascade on delete cascade\n" +
+                    ");\n" +
+                    "\n" +
+                    "create unique index  if not exists room_roomName_uindex\n" +
+                    "\ton room (roomName);\n" +
+                    "\n" +
+                    "create table if not exists roomPoints\n" +
+                    "(\n" +
+                    "\tid integer\n" +
+                    "\t\tconstraint roomPoints_pk\n" +
+                    "\t\t\tprimary key,\n" +
+                    "\tx double(30),\n" +
+                    "\ty double(30),\n" +
+                    "\troomID_FK int\n" +
+                    "\t\treferences room\n" +
+                    "\t\t\ton update cascade on delete cascade\n" +
+                    ");\n" +
+                    "\n" +
+                    "create table if not exists graph\n" +
+                    "(\n" +
+                    "\troomPoint_FK int\n" +
+                    "\t\treferences roomPoints\n" +
+                    "\t\t\ton update cascade on delete cascade,\n" +
+                    "\tconnected_FK int\n" +
+                    "\t\treferences roomPoints\n" +
+                    "\t\t\ton update cascade on delete cascade,\n" +
+                    "\tconstraint graph_pk\n" +
+                    "\t\tprimary key (roomPoint_FK, connected_FK)\n" +
+                    ");\n" +
+                    "\n";
             Connection connection = ConnectionManager.getConnection();
             Statement statement = connection.createStatement();
             statement.executeUpdate(createQuery);
